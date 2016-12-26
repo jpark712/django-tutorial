8e8220706fbf1893fbc34cff93a368381caef797
Justin Park <jpark712@gmail.com>
2016-12-26 03:07:56 -0600

    [Part2] forgot to add django.utils timezone class

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 @python_2_unicode_compatible
 class Question(models.Model):
